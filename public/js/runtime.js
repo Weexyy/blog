59cb506a36cd8b713c35db0de1c26985720d2aec
Weexy <zhengweixin0101@outlook.com>
2024-07-08 18:26:15 +0800

    改进footer

--- public/js/runtime.js
+++ public/js/runtime.js
@@ -5,7 +5,7 @@ function updateRuntime() {
   const difference = Number(now) - Number(startDate); // 确保类型为 number
   
   const days = difference / (1000 * 60 * 60 * 24); // 计算运行的天数
-  const runtimeText = `本站已运行 ${days.toFixed(5)} 天`; // 保留5位小数，以秒级精度
+  const runtimeText = `${days.toFixed(5)}`; // 保留5位小数，以秒级精度
 
   const runtimeElement = document.getElementById('runtime');
   if (runtimeElement) { // 检查 runtimeElement 是否为 null
